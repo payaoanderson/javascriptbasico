2ded5a6b4eed3bfdea253fc2f6059e208e8e8090
anderson payao <andersonpayao018@gmail.com>
2023-11-14 21:58:33 -0300

    validação do digito x

--- 02-variaveis-operadores/04-formulario/script/valida-cpf.js
+++ 02-variaveis-operadores/04-formulario/script/valida-cpf.js
@@ -2,13 +2,38 @@ document.getElementById("validarbutton").
     addEventListener("click", function () {
         // alert("deu certo!!!");
         const cpf = document.getElementById("cpfinput").value;
-        if(validacpf(cpf)) {
+        if (validacpf(cpf)) {
             document.getElementById("resultado").textContent = "cpf válido";
-        }else{
+        } else {
             document.getElementById("resultado").textContent = "cpf inválido";
         }
     })
 
 function validacpf(vrcpf) {
-    return vrcpf.length === 11 & !isNaN(vrcpf);
+    let soma = 0
+    soma += parseInt(vrcpf.charAt(0)) * 10;
+    soma += parseInt(vrcpf.charAt(1)) * 9;
+    soma += parseInt(vrcpf.charAt(2)) * 8
+    soma += parseInt(vrcpf.charAt(3)) * 7;
+    soma += parseInt(vrcpf.charAt(4)) * 6;
+    soma += parseInt(vrcpf.charAt(5)) * 5;
+    soma += parseInt(vrcpf.charAt(6)) * 4;
+    soma += parseInt(vrcpf.charAt(7)) * 3;
+    soma += parseInt(vrcpf.charAt(8)) * 2;
+
+
+    let digitox = soma % 11;
+    digitox = 11 - digitox;
+
+    if (digitox > 10) {
+        digitox = 0;
+    }
+
+    let digitovalido = true;
+    if (digitox !== parseInt(vrcpf.charAt(9))) {
+        digitovalido = false;
+    }
+    return digitovalido;
+
+
 }
