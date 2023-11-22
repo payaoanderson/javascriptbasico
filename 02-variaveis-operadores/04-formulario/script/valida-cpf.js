417dc21e13e10b0e6eca8868a89f9f60e08c8613
anderson payao <andersonpayao018@gmail.com>
2023-11-21 22:23:28 -0300

    cpf

--- 02-variaveis-operadores/04-formulario/script/valida-cpf.js
+++ 02-variaveis-operadores/04-formulario/script/valida-cpf.js
@@ -29,11 +29,63 @@ function validacpf(vrcpf) {
         digitox = 0;
     }
 
-    let digitovalido = true;
+
+
+    soma = 0;
+    soma += parseInt(vrcpf.charAt(0)) * 11;
+    soma += parseInt(vrcpf.charAt(1)) * 10;
+    soma += parseInt(vrcpf.charAt(2)) * 9;
+    soma += parseInt(vrcpf.charAt(3)) * 8;
+    soma += parseInt(vrcpf.charAt(4)) * 7;
+    soma += parseInt(vrcpf.charAt(5)) * 6;
+    soma += parseInt(vrcpf.charAt(6)) * 5;
+    soma += parseInt(vrcpf.charAt(7)) * 4;
+    soma += parseInt(vrcpf.charAt(8)) * 3;
+    soma += parseInt(vrcpf.charAt(9)) * 2;
+
+
+
+    let digitoy = soma % 11;
+    digitoy = 11 - digitoy;
+    if (digitoy >= 10) {
+        digitox = 0;
+    }
+
+    let digitvalido = true;
     if (digitox !== parseInt(vrcpf.charAt(9))) {
         digitovalido = false;
     }
+
+    if (digitoy !== parseInt(vrcpf.charAt(10))) {
+        digitovalido = false;
+    }
+
+    soma = 0;
+    for (let posicao = 0, pesoY = 11; posicao < 10; posicao++, pesoY--) {
+        soma += parseInt(vrcpf.charAt(posicao)) * pesoY;
+
+    }
+
+
+    let digitoY = 11 - (soma % 11);
+    if (digitoY >= 10) {
+        digitoY = 0;
+    }
+
+    if (digitoY !== parseInt(vrcpf.charAt(10))) {
+        digitovalido = false;
+    }
+
+
+
+
+
     return digitovalido;
+
+
+
+
+
 
 
 }
